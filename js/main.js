26985481d9844e9a7de793eefbdbaede26167383
ksnell19 <ksnell@Kristins-MacBook-Pro.local>
2018-03-08 01:06:53 -0800

    another commit

--- js/main.js
+++ js/main.js
@@ -1,70 +1,114 @@
-var mercury = {
-  name: 'mercury',
-  text: '',
-  img: 'https://space-facts.com/wp-content/uploads/mercury-transparent.png'
+var aquarius = {
+  name: 'aquarius',
+  date: 'January 20 - February 18',
+  element: 'Air sign',
+  text: 'STRENGTHS: Progressive, original, independent, humanitarian. \nWEAKNESSES: Runs from emotional expression, temperamental, uncompromising, aloof. \nLIKES: Fun with friends, helping others, fighting for causes, intellectual conversation, a good listener. \nDISLIKES: Limitations, broken promises, being lonely, dull or boring situations, people who disagree with them.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022008-3d-transparent-glass-icon-culture-astrology-aquarius.png'
 };
 
-var venus = {
-  name: 'venus',
-  text: '',
-  img: 'https://space-facts.com/wp-content/uploads/venus-transparent.png'
+var pisces = {
+  name: 'pisces',
+  date: 'February 19 - March 20',
+  element: 'Water sign',
+  text: 'STRENGTHS: Compassionate, artistic, intuitive, gentle, wise, musical. \nWEAKNESSES: Fearful, overly trusting, sad, desire to escape reality, can be a victim or a martyr. \nLIKES: Being alone, sleeping, music, romance, visual media, swimming, spiritual themes. \nDISLIKES: Know-it-all, being criticized, the past coming back to haunt, cruelty of any kind.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022015-3d-transparent-glass-icon-culture-astrology-pisces.png'
 };
 
-var earth = {
-  name: 'earth',
-  text: '',
-  img: 'https://space-facts.com/wp-content/uploads/earth-transparent.png'
+var aries = {
+  name: 'aries',
+  date: 'March 21 - April 19',
+  element: 'Fire sign',
+  text: 'STRENGTHS: Courageous, determined, confident, enthusiastic, optimistic, honest, passionate. \nWEAKNESSES: Impatient, moody, short-tempered, impulsive, aggressive. \nLIKES: Comfortable clothes, taking on leadership roles, physical challenges, individual sports. \nDISLIKES: Inactivity, delays, work that does not use their talents.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022009-3d-transparent-glass-icon-culture-astrology-aries.png'
 };
 
-var mars = {
-  name: 'mars',
-  text: '',
-  img: 'https://space-facts.com/wp-content/uploads/mars-transparent.png'
+var taurus = {
+  name: 'taurus',
+  date: 'April 20 - May 20',
+  element: 'Earth sign',
+  text: 'STRENGTHS: Reliable, patient, practical, devoted, responsible, stable. \nWEAKNESSES: Stubborn, possessive, uncompromising. \nLIKES: Gardening, cooking, music, romance, high quality clothes, working with hands.\nDISLIKES: Sudden changes, complications, insecurity of any kind, synthetic fabrics.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022018-3d-transparent-glass-icon-culture-astrology-taurus.png'
 };
-var jupiter = {
-  name: 'jupiter',
-  text: '',
-  img: 'https://space-facts.com/wp-content/uploads/jupiter-transparent.png'
+var gemini = {
+  name: 'gemini',
+  date: 'May 21 - June 20',
+  element: 'Air sign',
+  text: 'STRENGTHS: Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas. \nWEAKNESSES: Nervous, inconsistent, indecisive. \nLIKES: Music, books, magazines, chats with nearly anyone, short trips around the town. \nDISLIKES: Being alone, being confined, repetition and routine.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022012-3d-transparent-glass-icon-culture-astrology-gemini.png'
 };
-var saturn = {
-  name: 'saturn',
-  text: '',
-  img: 'https://space-facts.com/wp-content/uploads/saturn.png'
-};
-var uranus = {
-  name: 'uranus',
-  text: '',
-  img: 'https://space-facts.com/wp-content/uploads/uranus-transparent.png'
-};
-var neptune = {
-  name: 'neptune',
+var cancer = {
+  name: 'cancer',
+  date: 'June 21 - July 22',
+  element: 'Water sign',
   text: 'STRENGTHS: Tenacious, highly imaginative, loyal, emotional, sympathetic, persuasive. \nWEAKNESSES: Moody, pessimistic, suspicious, manipulative, insecure. \nLIKES: Art, home-based hobbies, relaxing near or in water, helping loved ones, a good meal with friends. \nDISLIKES: Strangers, any criticism of Mom, revealing of personal life.',
-  img: 'https://space-facts.com/wp-content/uploads/neptune-transparent.png'
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022010-3d-transparent-glass-icon-culture-astrology-cancer.png'
+};
+var leo = {
+  name: 'leo',
+  date: 'July 23 - August 22',
+  element: 'Fire sign',
+  text: 'STRENGTHS: Creative, passionate, generous, warm-hearted, cheerful, humorous. \nWEAKNESSES: Arrogant, stubborn, self-centered, lazy, inflexible. \nLIKES: Theater, taking holidays, being admired, expensive things, bright colors, fun with friends. \nDISLIKES: Being ignored, facing difficult reality, not being treated like a king or queen.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022013-3d-transparent-glass-icon-culture-astrology-leo.png'
+};
+var virgo = {
+  name: 'virgo',
+  date: 'August 23 - September 22',
+  element: 'Earth sign',
+  text: 'STRENGTHS: Loyal, analytical, kind, hardworking, practical. \nWEAKNESSES: Shyness, worry, overly critical of self and others, all work and no play. \nLIKES: Animals, healthy food, books, nature, cleanliness. \nDISLIKES: Rudeness, asking for help, taking center stage.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022019-3d-transparent-glass-icon-culture-astrology-virgo.png'
+};
+var libra = {
+  name: 'libra',
+  date: 'September 23 - October 22',
+  element: 'Air sign',
+  text: 'STRENGTHS: Cooperative, diplomatic, gracious, fair-minded, social. \nWEAKNESSES: Indecisive, avoids confrontations, will carry a grudge, self-pity. \nLIKES: Harmony, gentleness, sharing with others, the outdoors.\nDISLIKES: Violence, injustice, loudmouths, conformity.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022014-3d-transparent-glass-icon-culture-astrology-libra.png'
+};
+var scorpio = {
+  name: 'scorpio',
+  date: 'October 23 - November 21',
+  element: 'Water sign',
+  text: 'STRENGTHS: Resourceful, brave, passionate, stubborn, a true friend. \nWEAKNESSES: Distrusting, jealous, secretive, violent. \nLIKES: Truth, facts, being right, longtime friends, teasing, a grand passion. \nDISLIKES: Dishonesty, revealing secrets, passive people.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022017-3d-transparent-glass-icon-culture-astrology-scorpio.png'
+};
+var sagittarius = {
+  name: 'sagittarius',
+  date: 'November 22 - December 21',
+  element: 'Fire sign',
+  text: 'STRENGTHS: Generous, idealistic, great sense of humor. \nWEAKNESSES: Promises more than can deliver, very impatient, will say anything no matter how undiplomatic. \nLIKES: Freedom, travel, philosophy, being outdoors. \nDISLIKES: Clingy people, being constrained, off-the-wall theories, details.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022016-3d-transparent-glass-icon-culture-astrology-sagittarius.png'
+};
+var capricorn = {
+  name: 'capricorn',
+  date: 'December 22 - January 19',
+  element: 'Earth sign',
+  text: 'STRENGTHS: Responsible, disciplined, self-control, good managers. \nWEAKNESSES: Know-it-all, unforgiving, condescending, expecting the worst. \nLIKES: Family, tradition, music, understated status, quality craftsmanship. \nDISLIKES: Almost everything at some point.',
+  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022011-3d-transparent-glass-icon-culture-astrology-capricorn.png'
 };
 
 
 //array of objects
-var planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
+var zodiac = [aquarius, pisces, aries, taurus, gemini, cancer, leo, virgo, libra, scorpio, sagittarius, capricorn];
 
 //write the click function first
-function showInfo(){
+function showSign(){
 //remove whats in div in first place. (Why is there something there?)
-document.getElementById('planet-names').innerHTML= '' ;
+document.getElementById('zodiac-sign').innerHTML= '' ;
 
    //grab the input
-   var userInput = document.getElementById('planet').value;
+   var userInput = document.getElementById('sign').value;
    console.log(userInput);
 
  //match input to one of our objects
  //loop through and return a match
- for(var i=0; i<planet.length; i++) {
-   if(userInput.toLowerCase() === planet[i].name) { //do we have a match?
+ for(var i=0; i<zodiac.length; i++) {
+   if(userInput.toLowerCase() === zodiac[i].name) { //do we have a match?
 
    var img = document.createElement('img');
    var head = document.createElement('h4');
    var elements= document.createElement('h5');
    var p = document.createElement('p');
-   // p.setAttribute('style', 'width: 600px; margin: 0 auto; padding: 30px; border: 1px solid #868e96; border-radius: 10px; text-align: left');
+   p.setAttribute('style', 'width: 600px; margin: 0 auto; padding: 30px; border: 1px solid #868e96; border-radius: 10px; text-align: left');
    var div = document.createElement('div');
 
    //created elements with the zodiac info
@@ -78,7 +122,6 @@ document.getElementById('planet-names').innerHTML= '' ;
    div.appendChild(head);
    div.appendChild(elements);
    div.appendChild(p);
-
 
    //display to user
    var zodiacSign = document.getElementById('zodiac-sign');
