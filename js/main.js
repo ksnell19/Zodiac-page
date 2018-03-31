43c1021dd6627d32c70fc9d6afe5574b58b29256
Kristin <31809080+ksnell19@users.noreply.github.com>
2018-03-31 02:33:05 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var aquarius = {
   date: 'January 20 - February 18',
   element: 'Air sign',
   text: 'STRENGTHS: Progressive, original, independent, humanitarian. \nWEAKNESSES: Runs from emotional expression, temperamental, uncompromising, aloof. \nLIKES: Fun with friends, helping others, fighting for causes, intellectual conversation, a good listener. \nDISLIKES: Limitations, broken promises, being lonely, dull or boring situations, people who disagree with them.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022008-3d-transparent-glass-icon-culture-astrology-aquarius.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/aquarius.png?raw=true'
 };
 
 var pisces = {
@@ -11,7 +11,7 @@ var pisces = {
   date: 'February 19 - March 20',
   element: 'Water sign',
   text: 'STRENGTHS: Compassionate, artistic, intuitive, gentle, wise, musical. \nWEAKNESSES: Fearful, overly trusting, sad, desire to escape reality, can be a victim or a martyr. \nLIKES: Being alone, sleeping, music, romance, visual media, swimming, spiritual themes. \nDISLIKES: Know-it-all, being criticized, the past coming back to haunt, cruelty of any kind.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022015-3d-transparent-glass-icon-culture-astrology-pisces.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/pisces.png?raw=true',
 };
 
 var aries = {
@@ -19,7 +19,7 @@ var aries = {
   date: 'March 21 - April 19',
   element: 'Fire sign',
   text: 'STRENGTHS: Courageous, determined, confident, enthusiastic, optimistic, honest, passionate. \nWEAKNESSES: Impatient, moody, short-tempered, impulsive, aggressive. \nLIKES: Comfortable clothes, taking on leadership roles, physical challenges, individual sports. \nDISLIKES: Inactivity, delays, work that does not use their talents.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022009-3d-transparent-glass-icon-culture-astrology-aries.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/aries.png?raw=true'
 };
 
 var taurus = {
@@ -27,63 +27,63 @@ var taurus = {
   date: 'April 20 - May 20',
   element: 'Earth sign',
   text: 'STRENGTHS: Reliable, patient, practical, devoted, responsible, stable. \nWEAKNESSES: Stubborn, possessive, uncompromising. \nLIKES: Gardening, cooking, music, romance, high quality clothes, working with hands.\nDISLIKES: Sudden changes, complications, insecurity of any kind, synthetic fabrics.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022018-3d-transparent-glass-icon-culture-astrology-taurus.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/taurus.png?raw=true'
 };
 var gemini = {
   name: 'gemini',
   date: 'May 21 - June 20',
   element: 'Air sign',
   text: 'STRENGTHS: Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas. \nWEAKNESSES: Nervous, inconsistent, indecisive. \nLIKES: Music, books, magazines, chats with nearly anyone, short trips around the town. \nDISLIKES: Being alone, being confined, repetition and routine.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022012-3d-transparent-glass-icon-culture-astrology-gemini.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/gemini.png?raw=true'
 };
 var cancer = {
   name: 'cancer',
   date: 'June 21 - July 22',
   element: 'Water sign',
   text: 'STRENGTHS: Tenacious, highly imaginative, loyal, emotional, sympathetic, persuasive. \nWEAKNESSES: Moody, pessimistic, suspicious, manipulative, insecure. \nLIKES: Art, home-based hobbies, relaxing near or in water, helping loved ones, a good meal with friends. \nDISLIKES: Strangers, any criticism of Mom, revealing of personal life.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022010-3d-transparent-glass-icon-culture-astrology-cancer.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/cancer.png?raw=true'
 };
 var leo = {
   name: 'leo',
   date: 'July 23 - August 22',
   element: 'Fire sign',
   text: 'STRENGTHS: Creative, passionate, generous, warm-hearted, cheerful, humorous. \nWEAKNESSES: Arrogant, stubborn, self-centered, lazy, inflexible. \nLIKES: Theater, taking holidays, being admired, expensive things, bright colors, fun with friends. \nDISLIKES: Being ignored, facing difficult reality, not being treated like a king or queen.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022013-3d-transparent-glass-icon-culture-astrology-leo.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/leo.png?raw=true'
 };
 var virgo = {
   name: 'virgo',
   date: 'August 23 - September 22',
   element: 'Earth sign',
   text: 'STRENGTHS: Loyal, analytical, kind, hardworking, practical. \nWEAKNESSES: Shyness, worry, overly critical of self and others, all work and no play. \nLIKES: Animals, healthy food, books, nature, cleanliness. \nDISLIKES: Rudeness, asking for help, taking center stage.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022019-3d-transparent-glass-icon-culture-astrology-virgo.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/virgo.png?raw=true'
 };
 var libra = {
   name: 'libra',
   date: 'September 23 - October 22',
   element: 'Air sign',
   text: 'STRENGTHS: Cooperative, diplomatic, gracious, fair-minded, social. \nWEAKNESSES: Indecisive, avoids confrontations, will carry a grudge, self-pity. \nLIKES: Harmony, gentleness, sharing with others, the outdoors.\nDISLIKES: Violence, injustice, loudmouths, conformity.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022014-3d-transparent-glass-icon-culture-astrology-libra.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/libra.png?raw=true'
 };
 var scorpio = {
   name: 'scorpio',
   date: 'October 23 - November 21',
   element: 'Water sign',
   text: 'STRENGTHS: Resourceful, brave, passionate, stubborn, a true friend. \nWEAKNESSES: Distrusting, jealous, secretive, violent. \nLIKES: Truth, facts, being right, longtime friends, teasing, a grand passion. \nDISLIKES: Dishonesty, revealing secrets, passive people.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022017-3d-transparent-glass-icon-culture-astrology-scorpio.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/scorpio.png?raw=true'
 };
 var sagittarius = {
   name: 'sagittarius',
   date: 'November 22 - December 21',
   element: 'Fire sign',
   text: 'STRENGTHS: Generous, idealistic, great sense of humor. \nWEAKNESSES: Promises more than can deliver, very impatient, will say anything no matter how undiplomatic. \nLIKES: Freedom, travel, philosophy, being outdoors. \nDISLIKES: Clingy people, being constrained, off-the-wall theories, details.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022016-3d-transparent-glass-icon-culture-astrology-sagittarius.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/sagittarius.png?raw=true'
 };
 var capricorn = {
   name: 'capricorn',
   date: 'December 22 - January 19',
   element: 'Earth sign',
   text: 'STRENGTHS: Responsible, disciplined, self-control, good managers. \nWEAKNESSES: Know-it-all, unforgiving, condescending, expecting the worst. \nLIKES: Family, tradition, music, understated status, quality craftsmanship. \nDISLIKES: Almost everything at some point.',
-  img: 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-culture/022011-3d-transparent-glass-icon-culture-astrology-capricorn.png'
+  img: 'https://github.com/ksnell19/Zodiac-page/blob/master/img/capricorn.png?raw=true'
 };
 
 
